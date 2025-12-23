17c1c75c8776c58332f1ecc1f8ac83944a97a26e
Ntshimane <jmahlaola@gmail.com>
2025-12-23 04:39:49 +0200

    Update clients.vue

--- app/pages/clients.vue
+++ app/pages/clients.vue
@@ -38,7 +38,7 @@
               <div class="flex items-center mb-6">
                 <div class="w-12 h-12 gradient-bg rounded-full flex items-center justify-center text-white font-bold mr-4">SM</div>
                 <div>
-                  <h4 class="font-bold text-gray-800 dark:text-white">Sarah Moloi</h4>
+                  <h4 class="font-bold text-gray-800 dark:text-white">Thokozile Mahlaola</h4>
                   <p class="text-gray-600 dark:text-gray-400 text-sm">CEO, The Cherry Blue House</p>
                 </div>
               </div>
